ab015d95fd0d349ef968992db8f738bf28e4d69a
gaurav-barclays <gaurav.simply@gmail.com>
2017-11-02 06:10:59 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -10,16 +10,17 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Your solution code here
 
-def rf_rfe(data):
-    X = data.iloc[:,:-1]
-   # print(X)
-    Y = data['SalePrice']
+def rf_rfe(df):
+    X = df.iloc[:,:-1]
+    #print(X)
+    Y = df['SalePrice']
     model = RandomForestClassifier()
-    rfe = RFE(model, n_features_to_select=18)
-    fitmod = rfe.fit(X, Y)
+    rfe = RFE(model, n_features_to_select=17)
+    fit = rfe.fit(X, Y)
+    #print("Num Features: %d") % fit.n_features_
+    return X.columns.values[fit.support_].tolist()
+    #print("Selected Features: %s") % fit.support_
+    #print("Feature Ranking: %s") % fit.ranking_
 
-    return X.columns.values[fitmod.support_].tolist()
-
-
-#a=rf_rfe(data)
-#print((a))
+#l=rf_rfe(data)
+#print(type(l))
--- q05_forward_selected/build.py
+++ q05_forward_selected/build.py
@@ -1,3 +1,4 @@
+# %load q05_forward_selected/build.py
 # Default imports
 import pandas as pd
 from sklearn.linear_model import LinearRegression
@@ -6,8 +7,10 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 model = LinearRegression()
 
-
+#print(data.info())
 # Your solution code here
+def forward_selected(data, model):
+    features = data.iloc[:,-1]
+    print(len(features.column.values))
 
-
-
+forward_selected(data, model)
