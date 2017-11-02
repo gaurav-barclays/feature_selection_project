d6043aa02ec3280fe5c941c5954859a11c6c5381
gaurav-barclays <gaurav.simply@gmail.com>
2017-11-02 04:10:05 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -9,4 +10,16 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Your solution code here
 
+def rf_rfe(data):
+    X = data.iloc[:,:-1]
+   # print(X)
+    Y = data['SalePrice']
+    model = RandomForestClassifier()
+    rfe = RFE(model, n_features_to_select=18)
+    fitmod = rfe.fit(X, Y)
 
+    return X.columns.values[fitmod.support_].tolist()
+
+
+#a=rf_rfe(data)
+#print((a))
--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,10 +1,23 @@
+
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFE
 import pandas as pd
 import numpy as np
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
-
+np.random.seed(9)
+def select_from_model (data):
+    X = data.iloc[:,:-1]
+    Y = data['SalePrice']
+   # print(Y)
+    model = RandomForestClassifier()
+    rfe = SelectFromModel(model)
+    fitmod = rfe.fit(X, Y)
+    return X.columns.values[fitmod.get_support()].tolist()
 
 # Your solution code here
+#a=select_from_model(data)
+#print((a))
